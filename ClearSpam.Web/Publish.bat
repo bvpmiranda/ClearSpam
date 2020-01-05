aee5001c0974af1ccf796c941c3915ff6ed7ff2b
Bruno Miranda <bvpmiranda@mac.com>
2020-01-04 18:40:51 -0800

    Adds support to publishing to heroku

---
+++ ClearSpam.Web/Publish.bat
@@ -0,0 +1,16 @@
+﻿@ECHO OFF
+
+del .\bin\release\netcoreapp2.2\publish\appsettings.json
+del .\bin\release\netcoreapp2.2\publish\appsettings.notebruno.json
+del .\bin\release\netcoreapp2.2\publish\appsettings.zenBookFlip.json
+ren .\bin\release\netcoreapp2.2\publish\appsettings.production.json appsettings.json 
+
+docker build -t clearspam.web ./bin/release/netcoreapp2.2/publish
+
+docker tag clearspam.web registry.heroku.com/clearspam/web
+
+docker push registry.heroku.com/clearspam/web
+
+heroku container:release web -a clearspam
+
+PAUSE
